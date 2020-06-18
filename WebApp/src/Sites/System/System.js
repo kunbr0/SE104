c541ff8d6634fd33cbfa245ebad202e7187cb080
Nguyen Nhat Long <nhtlonga@gmail.com>
2020-06-18 13:33:06 +0700

    change language

--- WebApp/src/Sites/System/System.js
+++ WebApp/src/Sites/System/System.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import { Menu, Dropdown , message } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import "./System.css"
+import {useTranslation} from 'react-i18next';
+
+
 
 const SSystem = () => {
+
+    const { i18n } = useTranslation();
+const handleChangeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+}
+
+
+const onClick = ({ key }) => {
+    //message.info(`Click on item ${key}`);
+    handleChangeLanguage(key);
+};
+
+const menu = (
+    <Menu onClick={onClick}>
+      <Menu.Item key="en">
+        <a target="_blank" rel="noopener noreferrer">
+          English
+        </a>
+      </Menu.Item>
+      <Menu.Item key="vi">
+        <a target="_blank" rel="noopener noreferrer">
+          Vietnamese
+        </a>
+      </Menu.Item>
+      <Menu.Item key="cn">
+        <a target="_blank" rel="noopener noreferrer">
+          Chinese
+        </a>
+      </Menu.Item>
+      <Menu.Item danger>blah</Menu.Item>
+    </Menu>
+  );
+
+
+
     return (
-        <div>2e1223123</div>
+        <Dropdown overlay={menu}>
+            <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+            Language <DownOutlined />
+            </a>
+        </Dropdown>
     );
 }
 
