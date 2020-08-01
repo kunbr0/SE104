3cbe56398a0905a03c7162f125a6c594932d9333
KuN <mac@toan18520383.local>
2020-08-01 22:36:07 +0700

    custom Profile Page

--- WebApp/src/Containers/Profile/Profile.js
+++ WebApp/src/Containers/Profile/Profile.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import { Descriptions } from 'antd';
+import React, {useState, useEffect} from 'react';
+import { Descriptions, Badge, Card } from 'antd';
 
 const Profile = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(()=>{
+        setTimeout(()=>{
+            setIsLoading(false);
+        }, 1000);
+    });
     return(
-        <Descriptions title="User Info" layout="vertical">
-            <Descriptions.Item label="UserName">kunbr0</Descriptions.Item>
-            <Descriptions.Item label="Telephone">1800 1234</Descriptions.Item>
-            <Descriptions.Item label="Live">Phu Yen, VietNam</Descriptions.Item>
-            <Descriptions.Item label="Address" span={1}>
-            No. 18, LVC, Hangzhou, Zhejiang, China
+        <Card loading={isLoading} style={{width : "70%"}}>
+        <Descriptions title="User Info" layout="vertical" bordered column={10}>
+            <Descriptions.Item label="UserId" span={2}>trungnv</Descriptions.Item>
+            <Descriptions.Item label="Name" span={4}>Nguyen Van Trung</Descriptions.Item>
+            <Descriptions.Item label="Status" span={4}>
+            <Badge status="processing" text="Working" />
             </Descriptions.Item>
-            <Descriptions.Item label="Remark">Nong Dan</Descriptions.Item>
+
+            <Descriptions.Item label="Falcuty" span={4}>Software Engineering</Descriptions.Item>
+            
+            <Descriptions.Item label="Email" span={4}>
+            trungnv@uit.edu.vn
+            </Descriptions.Item>
+            <Descriptions.Item label="National ID" span={2}>07920128123</Descriptions.Item>
+            
+            <Descriptions.Item label="Address" span={6}>173/22 Nguyen Thi Minh Khai, P6, Q3, TP.HCM</Descriptions.Item>
+            <Descriptions.Item label="Date of birth" span={2}>17/08/1972</Descriptions.Item>
+            <Descriptions.Item label="Gender" span={2}>Male</Descriptions.Item>
+            
+            <Descriptions.Item label="Classes">
+            SE104.K22.PMCL
+            <br />
+            IT002.K23.PMCL
+            <br />
+            IT007.K18.KHCL
+            <br />
+            SE101.K13.PMCL
+            <br />
+            PH002.K25
+            
+            </Descriptions.Item>
         </Descriptions>
+        </Card>
     );
 }
 
