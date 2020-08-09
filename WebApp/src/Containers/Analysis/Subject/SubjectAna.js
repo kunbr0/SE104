5ffbf49f32c2afa8aaabbc2df8b7eec9dad5a467
KuN <toantanqn@gmail.com>
2020-08-09 11:38:30 +0700

    Remove TextTranslation for mobile res

--- WebApp/src/Containers/Analysis/Subject/SubjectAna.js
+++ WebApp/src/Containers/Analysis/Subject/SubjectAna.js
@@ -1,57 +1,69 @@
-import React, {useEffect, useState} from 'react';
-import { Table } from 'antd';
-import TextTranslation from '../../../Components/TextTranslation/TextTranslation';
+import React, { useEffect, useState } from "react";
+import { Table } from "antd";
+import TextTranslation from "../../../Components/TextTranslation/TextTranslation";
 
+import { useSSR } from "react-i18next";
 
-import { useSSR } from 'react-i18next';
-
-import './SubjectAna.css';
-
-
-
+import "./SubjectAna.css";
 
 const columnsOfStudentDetails = [
-    {
-        title: 
-            <div>
-                <TextTranslation textName="ClassInfo-Table-No.1" kClass="pcview"/>
-                <TextTranslation textName="ClassInfo-Table-No.2" kClass="mbview"/>
-            </div>,
-        dataIndex: 'key',
-        key: 'key',
-        width: 8,
-        fixed: 'left',
-        sorter: (a, b) => a.key - b.key,
-    },
-    
-    {
-        title: <TextTranslation textName="ClassInfo-TopBar-Class.1" kClass="pcview"/>,
-        dataIndex: 'name',
-        key: 'name',
-        width: 25,
-        sorter: (a, b) => a.name.split(' ').slice(-1)[0].localeCompare(b.name.split(' ').slice(-1)[0])
-    },
-    {
-    title: <TextTranslation textName="ClassInfo-Table-EnrolledStudents.1" kClass="pcview"/>,
-    dataIndex: 'siso',
-    key: 'siso',
-    width: 12,
-    },
-    {
-    title: <TextTranslation textName="ClassInfo-Table-NumOfPass.1" kClass="pcview"/>,
-    dataIndex: 'sldat',
-    key: 'sldat',
-    width: 12,
-    },
-    {
-    title: <TextTranslation textName="ClassInfo-Table-Percentage.1" kClass="pcview"/>,
-    dataIndex: 'tyle',
-    key: 'tyle',
-    width: 12,
-    }    
-      
-];
+  {
+    title: (
+      <div>
+        <TextTranslation textName="ClassInfo-Table-No.1" kClass="pcview" />
+      </div>
+    ),
+    dataIndex: "key",
+    key: "key",
+    width: 8,
+    fixed: "left",
+    sorter: (a, b) => a.key - b.key,
+  },
 
+  {
+    title: (
+      <TextTranslation textName="ClassInfo-TopBar-Class.1" kClass="pcview" />
+    ),
+    dataIndex: "name",
+    key: "name",
+    width: 25,
+    sorter: (a, b) =>
+      a.name
+        .split(" ")
+        .slice(-1)[0]
+        .localeCompare(b.name.split(" ").slice(-1)[0]),
+  },
+  {
+    title: (
+      <TextTranslation
+        textName="ClassInfo-Table-EnrolledStudents.1"
+        kClass="pcview"
+      />
+    ),
+    dataIndex: "siso",
+    key: "siso",
+    width: 12,
+  },
+  {
+    title: (
+      <TextTranslation textName="ClassInfo-Table-NumOfPass.1" kClass="pcview" />
+    ),
+    dataIndex: "sldat",
+    key: "sldat",
+    width: 12,
+  },
+  {
+    title: (
+      <TextTranslation
+        textName="ClassInfo-Table-Percentage.1"
+        kClass="pcview"
+      />
+    ),
+    dataIndex: "tyle",
+    key: "tyle",
+    width: 12,
+  },
+];
 
 // for (let i = 0; i < 100; i++) {
 //   data.push({
@@ -68,98 +80,73 @@ const columnsOfStudentDetails = [
 //   });
 // }
 
-
-
-
 // Map of changed row to push
 
-
 const StudentScoresTable = (props) => {
-    
+  const [classDetailsData, setClassDetailsData] = useState([{}]);
+  const [tableData, setTableData] = useState([]);
+  const [tableEditable, setTableEditable] = useState(false);
 
-    const [classDetailsData, setClassDetailsData] = useState([{}]);
-    const [tableData, setTableData] = useState([]);
-    const [tableEditable, setTableEditable] = useState(false);
-    
-    const [listOfChangedRows, setListOfChangedRows] = useState({});
+  const [listOfChangedRows, setListOfChangedRows] = useState({});
 
-    useEffect(()=>{
-        if(props.tableEditable !== tableEditable){
-            if(props.tableEditable === false){
-                setListOfChangedRows({});
-            }
-            setTableEditable(props.tableEditable);
-        }
-    });
+  useEffect(() => {
+    if (props.tableEditable !== tableEditable) {
+      if (props.tableEditable === false) {
+        setListOfChangedRows({});
+      }
+      setTableEditable(props.tableEditable);
+    }
+  });
 
+  const renderClassDetailsData = () => {
+    let data = []; // clear data
+    let i = 1;
 
-   
+    console.log(classDetailsData);
 
-
-    const renderClassDetailsData = () => {
-
-        let data = []; // clear data
-        let i=1;
-
-        console.log(classDetailsData);
-
-        if(classDetailsData){
-            for(let e of classDetailsData){
-                let a = {};
-                a = {...e}
-                a["key"] = i;
-                a["tyle"] = `${Math.floor(a["tyle"]*100)/100} %`
-                i++;
-                data.push(a);
-                
-
-            }
-            
-                
-        }
-        
-        setTableData(data);
-        
+    if (classDetailsData) {
+      for (let e of classDetailsData) {
+        let a = {};
+        a = { ...e };
+        a["key"] = i;
+        a["tyle"] = `${Math.floor(a["tyle"] * 100) / 100} %`;
+        i++;
+        data.push(a);
+      }
     }
 
-    
+    setTableData(data);
+  };
 
-    
-    useEffect(()=>{
+  useEffect(() => {
+    if (props.classDetailsData !== classDetailsData)
+      setClassDetailsData(props.classDetailsData || []);
+  }, [props.classDetailsData]);
 
-        if(props.classDetailsData !== classDetailsData) setClassDetailsData(props.classDetailsData || []);
-       
-    },[props.classDetailsData]);
+  useEffect(() => {
+    renderClassDetailsData();
+  }, [classDetailsData, tableEditable]);
 
-    useEffect(()=>{
-        renderClassDetailsData();
-    },[classDetailsData, tableEditable]);
-
-    
-
-    console.log(props.classDetailsData);
-    console.log(tableData);
-    return (
-        <>
-        
-        <Table 
-            loading={props.isLoading || false}
-            columns={columnsOfStudentDetails}
-            dataSource={tableData}
-            bordered
-            size="middle"
-            scroll={{ x: 'calc(300px + 50%)', y: 240 }}
-            expandable={true}
-            onRow={(record, rowIndex) => {
-                return {
-                    className : (listOfChangedRows[record.studentID]) ? 'editedRow' : ''
-                };
-            }}
-        />
-
-        
-        </>
-    )
-}
+  console.log(props.classDetailsData);
+  console.log(tableData);
+  return (
+    <>
+      <Table
+        loading={props.isLoading || false}
+        columns={columnsOfStudentDetails}
+        dataSource={tableData}
+        bordered
+        size="middle"
+        scroll={{ x: "calc(300px + 50%)", y: 240 }}
+        expandable={true}
+        onRow={(record, rowIndex) => {
+          return {
+            className: listOfChangedRows[record.studentID] ? "editedRow" : "",
+          };
+        }}
+      />
+    </>
+  );
+};
 
 export default StudentScoresTable;
--- WebApp/src/Containers/ClassDetails/ScoreDetails/ScoreDetails.js
+++ WebApp/src/Containers/ClassDetails/ScoreDetails/ScoreDetails.js
@@ -1,76 +1,68 @@
-import React, {useEffect, useState} from 'react';
-import { Table, InputNumber } from 'antd';
-import TextTranslation from '../../../Components/TextTranslation/TextTranslation';
-import ScoreCoefficient from '../SubjectCoefficient.json';
-import './ScoreDetails.css';
-import { useSSR } from 'react-i18next';
-import SConfig from '../../../config.json';
-import { useHttpClient } from '../../../Hooks/http-hook';
-import { Card, Col, Row, message, Space, Collapse, Button } from 'antd';
-
-
+import React, { useEffect, useState } from "react";
+import { Table, InputNumber } from "antd";
+import TextTranslation from "../../../Components/TextTranslation/TextTranslation";
+import ScoreCoefficient from "../SubjectCoefficient.json";
+import "./ScoreDetails.css";
+import { useSSR } from "react-i18next";
+import SConfig from "../../../config.json";
+import { useHttpClient } from "../../../Hooks/http-hook";
+import { Card, Col, Row, message, Space, Collapse, Button } from "antd";
 
 const columnsOfSubjectDetails = [
   {
-    title: 
-        <>
-            <TextTranslation textName="ClassInfo-Table-No.1" kClass="pcview"/>
-            <TextTranslation textName="ClassInfo-Table-No.2" kClass="mbview"/>
-        </>,
-    dataIndex: 'key',
-    key: 'key',
+    title: (
+      <>
+        <TextTranslation textName="ClassInfo-Table-No.1" kClass="pcview" />{" "}
+      </>
+    ),
+    dataIndex: "key",
+    key: "key",
     width: 8,
-    fixed: 'left',
-    
-    },
-  
-    {
-        title: 
-            <>
-                <TextTranslation textName="ClassInfo-Table-Name.1" kClass="pcview"/>
-                <TextTranslation textName="ClassInfo-Table-Name.2" kClass="mbview"/>
-            </>,
-        dataIndex: 'Name',
-        key: 'Name',
-        width: 35,
-        sorter: (a, b) => a.ID - b.ID,
-    },
-    {
-    title: 
-        <>
-            <TextTranslation textName="ClassInfo-Table-15mins.1" kClass="pcview"/>
-            <TextTranslation textName="ClassInfo-Table-15mins.2" kClass="mbview"/>
-        </>,
-    dataIndex: 'exam_1',
-    key: 'exam_1',
+    fixed: "left",
+  },
+
+  {
+    title: (
+      <>
+        <TextTranslation textName="ClassInfo-Table-Name.1" kClass="pcview" />{" "}
+      </>
+    ),
+    dataIndex: "Name",
+    key: "Name",
+    width: 35,
+    sorter: (a, b) => a.ID - b.ID,
+  },
+  {
+    title: (
+      <>
+        <TextTranslation textName="ClassInfo-Table-15mins.1" kClass="pcview" />{" "}
+      </>
+    ),
+    dataIndex: "exam_1",
+    key: "exam_1",
     width: 12,
-    },
-    {
-    title: 
-        <>
-            <TextTranslation textName="ClassInfo-Table-45mins.1" kClass="pcview"/>
-            <TextTranslation textName="ClassInfo-Table-45mins.2" kClass="mbview"/>
-        </>,
-    dataIndex: 'exam_2',
-    key: 'exam_2',
+  },
+  {
+    title: (
+      <>
+        <TextTranslation textName="ClassInfo-Table-45mins.1" kClass="pcview" />{" "}
+      </>
+    ),
+    dataIndex: "exam_2",
+    key: "exam_2",
     width: 12,
-    },
-    {
-        title: 
-            <>
-                <TextTranslation textName="ClassInfo-Table-final.1" kClass="pcview"/>
-                <TextTranslation textName="ClassInfo-Table-final.2" kClass="mbview"/>
-            </>,
-        dataIndex: 'final',
-        key: 'final',
-        width: 12,
-    },     
-    
+  },
+  {
+    title: (
+      <>
+        <TextTranslation textName="ClassInfo-Table-final.1" kClass="pcview" />{" "}
+      </>
+    ),
+    dataIndex: "final",
+    key: "final",
+    width: 12,
+  },
 ];
-
-
-
-
 
 // for (let i = 0; i < 100; i++) {
 //   data.push({
@@ -87,108 +79,98 @@ const columnsOfSubjectDetails = [
 //   });
 // }
 
-
 const calculateFinalScore = (e) => {
-    
-    let coef = ScoreCoefficient;
-    return e.exam_1*1*coef.exam_1 + e.exam_2*1*coef.exam_2;
-}
-
+  let coef = ScoreCoefficient;
+  return e.exam_1 * 1 * coef.exam_1 + e.exam_2 * 1 * coef.exam_2;
+};
 
 // Map of changed row to push
 
-
 const StudentScoresTable = (props) => {
-    
+  const [classDetailsData, setClassDetailsData] = useState([{}]);
+  const [tableData, setTableData] = useState([]);
+  const [tableEditable, setTableEditable] = useState(false);
 
-    const [classDetailsData, setClassDetailsData] = useState([{}]);
-    const [tableData, setTableData] = useState([]);
-    const [tableEditable, setTableEditable] = useState(false);
-    
-    const [listOfChangedRows, setListOfChangedRows] = useState({});
+  const [listOfChangedRows, setListOfChangedRows] = useState({});
 
-    const { sendRequest } = useHttpClient();
-    const sleeper = (ms) => {
-        return function(x) {
-          return new Promise(resolve => setTimeout(() => resolve(x), ms));
-        };
-    }
-
-    const updateScore = (payload) => {
-        let urlRequest = `${SConfig.SERVER_URL}:${SConfig.SERVER_PORT}${SConfig.ClassRoutes.UpdateTranscripts}`;
-        sendRequest(urlRequest, "POST", payload,{
-            'Content-Type': 'application/json'
-        })
-        .then((response) => {
-            return response.json();
-        })
-
-        .then(sleeper(500))
-
-        .then((data) => {
-            console.log(data);
-            message.success(`Updated successfully !`);
-            props.callbackUpdateSuccess();
-        })
-        
-        .catch((error) => {
-            console.log(error);
-            message.error(`Updated Failed !`);
-            
-        });
+  const { sendRequest } = useHttpClient();
+  const sleeper = (ms) => {
+    return function (x) {
+      return new Promise((resolve) => setTimeout(() => resolve(x), ms));
     };
+  };
 
-    useEffect(()=>{
-        if(props.tableEditable !== tableEditable){
-            if(props.tableEditable === false){
-                if(Object.keys(listOfChangedRows).length > 0){
-                    let arrayToUpdate = [];
-                    Object.keys(listOfChangedRows).forEach(e=>{
-                        arrayToUpdate.push({
-                            ...listOfChangedRows[e],
-                            "student_id": listOfChangedRows[e].ID,
-                            "sem_name": listOfChangedRows[e].Semester,
-                            "subj_name": listOfChangedRows[e].Subject,
-                        });
-                    });
-                    console.log(arrayToUpdate);
-                    updateScore(arrayToUpdate);
-                }
-                setListOfChangedRows({});
-            }
-            setTableEditable(props.tableEditable);
+  const updateScore = (payload) => {
+    let urlRequest = `${SConfig.SERVER_URL}:${SConfig.SERVER_PORT}${SConfig.ClassRoutes.UpdateTranscripts}`;
+    sendRequest(urlRequest, "POST", payload, {
+      "Content-Type": "application/json",
+    })
+      .then((response) => {
+        return response.json();
+      })
+
+      .then(sleeper(500))
+
+      .then((data) => {
+        console.log(data);
+        message.success(`Updated successfully !`);
+        props.callbackUpdateSuccess();
+      })
+
+      .catch((error) => {
+        console.log(error);
+        message.error(`Updated Failed !`);
+      });
+  };
+
+  useEffect(() => {
+    if (props.tableEditable !== tableEditable) {
+      if (props.tableEditable === false) {
+        if (Object.keys(listOfChangedRows).length > 0) {
+          let arrayToUpdate = [];
+          Object.keys(listOfChangedRows).forEach((e) => {
+            arrayToUpdate.push({
+              ...listOfChangedRows[e],
+              student_id: listOfChangedRows[e].ID,
+              sem_name: listOfChangedRows[e].Semester,
+              subj_name: listOfChangedRows[e].Subject,
+            });
+          });
+          console.log(arrayToUpdate);
+          updateScore(arrayToUpdate);
         }
-    });
+        setListOfChangedRows({});
+      }
+      setTableEditable(props.tableEditable);
+    }
+  });
 
-
-    const updateListChangedRow = (kRow, key, value) => {
-        setListOfChangedRows(listOfChangedRows =>({
-            ...listOfChangedRows,
-            [kRow.ID] : (listOfChangedRows[kRow.ID] !== undefined) ?
-            {
-                ...listOfChangedRows[kRow.ID],
-                [key] : value
-            }:{
-                ...kRow,
-                [key] : value
+  const updateListChangedRow = (kRow, key, value) => {
+    setListOfChangedRows((listOfChangedRows) => ({
+      ...listOfChangedRows,
+      [kRow.ID]:
+        listOfChangedRows[kRow.ID] !== undefined
+          ? {
+              ...listOfChangedRows[kRow.ID],
+              [key]: value,
             }
-        }));
-    } 
+          : {
+              ...kRow,
+              [key]: value,
+            },
+    }));
+  };
 
+  const renderClassDetailsData = () => {
+    setTableData([]);
+    let data = []; // clear data
+    let i = 1;
 
-    const renderClassDetailsData = () => {
-
-        setTableData([]);
-        let data = []; // clear data
-        let i=1;
-        
-
-        if(classDetailsData){
-            for(let e of classDetailsData){
-                
-                //e["key"] = i;
-                e["final"] = calculateFinalScore(e);
-                /*e["final"] = (
+    if (classDetailsData) {
+      for (let e of classDetailsData) {
+        //e["key"] = i;
+        e["final"] = calculateFinalScore(e);
+        /*e["final"] = (
                 <Input
                     disabled={true}
                     value={calculateFinalScore(e)}
@@ -196,61 +178,59 @@ const StudentScoresTable = (props) => {
                     maxLength={25}
                 />);*/
 
-                let a = {};
-                Object.keys(e).forEach((key)=>{ 
-                    
-                    a[key] = (key === 'exam_1' || key === 'exam_2' || key === 'final' ) ? 
-                    <InputNumber
-                            style={{width: "100%"}}
-                            min={0} max={10}
-                            disabled={(key==="final")?true:!tableEditable}
-                            defaultValue={e[key] || ""}
-                            onChange={(value)=> updateListChangedRow(e, key, value)}
-                            step={0.5}
-                        /> : e[key]
-                    a["key"] = i;
-                });
-                data.push(a);
-                i++;
-            }
-        }
-        setTableData(data);
-        console.log(data);
+        let a = {};
+        Object.keys(e).forEach((key) => {
+          a[key] =
+            key === "exam_1" || key === "exam_2" || key === "final" ? (
+              <InputNumber
+                style={{ width: "100%" }}
+                min={0}
+                max={10}
+                disabled={key === "final" ? true : !tableEditable}
+                defaultValue={e[key] || ""}
+                onChange={(value) => updateListChangedRow(e, key, value)}
+                step={0.5}
+              />
+            ) : (
+              e[key]
+            );
+          a["key"] = i;
+        });
+        data.push(a);
+        i++;
+      }
     }
+    setTableData(data);
+    console.log(data);
+  };
 
-    
+  useEffect(() => {
+    if (props.classDetailsData !== classDetailsData)
+      setClassDetailsData(props.classDetailsData || []);
+  }, [props.classDetailsData]);
 
-    
-    useEffect(()=>{
+  useEffect(() => {
+    renderClassDetailsData();
+  }, [classDetailsData, tableEditable, props.classDetailsData]);
 
-        if(props.classDetailsData !== classDetailsData) setClassDetailsData(props.classDetailsData || []);
-       
-    },[props.classDetailsData]);
+  console.log(listOfChangedRows);
 
-    useEffect(()=>{
-        renderClassDetailsData();
-    },[classDetailsData, tableEditable, props.classDetailsData]);
-
-    
-
-    console.log(listOfChangedRows);
-    
-    return (
-        <Table 
-            loading={props.isLoading || false}
-            columns={columnsOfSubjectDetails}
-            dataSource={tableData}
-            bordered
-            size="middle"
-            scroll={{ x: 'calc(300px + 50%)', y: 240 }}
-            expandable={true}
-            onRow={(record, rowIndex) => {
-                return {
-                    className : (listOfChangedRows[record.ID]) ? 'editedRow' : ''
-                };
-            }}
-        />
-    )
-}
+  return (
+    <Table
+      loading={props.isLoading || false}
+      columns={columnsOfSubjectDetails}
+      dataSource={tableData}
+      bordered
+      size="middle"
+      scroll={{ x: "calc(300px + 50%)", y: 240 }}
+      expandable={true}
+      onRow={(record, rowIndex) => {
+        return {
+          className: listOfChangedRows[record.ID] ? "editedRow" : "",
+        };
+      }}
+    />
+  );
+};
 
 export default StudentScoresTable;
