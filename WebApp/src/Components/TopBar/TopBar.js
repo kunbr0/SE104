2ec4bc150a0c6b4f1d8f9490293d956d1d9aa88b
KuN <toantanqn@gmail.com>
2020-08-09 11:16:52 +0700

    fix DOB in AddStudent and errors in Translation

--- WebApp/src/Components/TopBar/TopBar.js
+++ WebApp/src/Components/TopBar/TopBar.js
@@ -1,48 +1,65 @@
-import React from 'react';
-import './TopBar.css';
-import { useHistory } from 'react-router-dom'
-import { Avatar, Badge, Menu, Dropdown } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-
+import React from "react";
+import "./TopBar.css";
+import { useHistory } from "react-router-dom";
+import { Avatar, Badge, Menu, Dropdown } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 const TopBar = () => {
+  let history = useHistory();
 
-    let history = useHistory();
-  
-    const logout = () => {
-        localStorage.removeItem("userData");
-        window.location.reload();
-    }
+  const logout = () => {
+    localStorage.removeItem("userData");
+    window.location.reload();
+  };
 
-    const menu = (
-        <Menu>
-            <Menu.Item key="0" >
-                <div onClick={()=>{history.push("/profile")}}>Profile</div>
-            </Menu.Item>
-            <Menu.Item key="1">
-                <div onClick={()=>{history.push("/system")}}>Settings</div>
-            </Menu.Item>
-            <Menu.Item key="2">
-                <div onClick={()=>{history.push("/activitylog")}}>Activity Log</div>
-            </Menu.Item>
-            <Menu.Divider />
-            <Menu.Item key="3" onClick={logout}>Logout</Menu.Item>
-        </Menu>
+  const menu = (
+    <Menu>
+      <Menu.Item key="0">
+        <div
+          onClick={() => {
+            history.push("/profile");
+          }}
+        >
+          Profile
+        </div>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <div
+          onClick={() => {
+            history.push("/system");
+          }}
+        >
+          Settings
+        </div>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <div
+          onClick={() => {
+            history.push("/activitylog");
+          }}
+        >
+          Activity Log
+        </div>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item key="3" onClick={logout}>
+        Logout
+      </Menu.Item>
+    </Menu>
   );
 
-    return(
-        
-        <div id="topbar-wrapper">
-            <Dropdown overlay={menu} trigger={['click']}>
-                <span className="avatar-item" id="topbar-avatar" >
-                    <Badge count={1}>
-                    <Avatar shape="square" icon={<UserOutlined />} />
-                    </Badge>
-                </span>
-            </Dropdown>
-        </div>
-    );
-    
-}
+  return (
+    <div id="topbar-wrapper">
+      <div id="topbar-title">Quản Lý Học Sinh</div>
+      <Dropdown overlay={menu} trigger={["click"]}>
+        <span className="avatar-item" id="topbar-avatar">
+          <Badge count={1}>
+            <Avatar shape="square" icon={<UserOutlined />} />
+          </Badge>
+        </span>
+      </Dropdown>
+    </div>
+  );
+};
 
 export default TopBar;
